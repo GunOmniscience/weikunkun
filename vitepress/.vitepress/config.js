a65ab77f88b9a3c616bf20201df4453240fcadad
jie <1927773824@qq.com>
2022-11-12 17:31:52 +0800

    部署

--- vitepress/.vitepress/config.js
+++ vitepress/.vitepress/config.js
@@ -1,6 +1,6 @@
 export default {
-    title: '韦坤堃', //站点标题
-    description: '韦坤堃',//mate标签description，多用于搜索引擎抓取摘要
+    title: '八大机械工业社', //站点标题
+    description: '八大机械工业社',//mate标签description，多用于搜索引擎抓取摘要
     lang: 'zh-cn',
     lastUpdated: true,
     base: '/weikunkun/',
